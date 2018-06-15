8cdd262413d24561715286d6023a2aef30e8ba33
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2018-06-15 13:08:56 -0400

    added example

---
+++ examples/example_mediation1.R
@@ -0,0 +1,50 @@
+##An example of mediation 1
+
+##install if you need to
+devtools::install_github("doomlab/MeMoBootR")
+
+##load the library
+library(MeMoBootR)
+
+saved = mediation1(y = "mpg", #DV
+                   x = "cyl", #IV
+                   m = "hp",  #Mediator
+                   cvs = c("drat", "gear"), #Any covariates
+                   df = mtcars, #Dataframe
+                   with_out = T, #Not required but can change to F for no outliers
+                   nboot = 1000, #Number of bootstraps
+                   conf_level = .95 #CI width
+                   )
+
+####view data screening####
+#outlier information is in the DF
+View(saved$datascreening$fulldata)
+
+#additivity
+saved$datascreening$correl
+
+#linearity
+saved$datascreening$linearity
+
+#normality
+saved$datascreening$normality
+
+#homogs
+saved$datascreening$homogen
+
+####view the analysis####
+summary(saved$model1) #c path
+summary(saved$model2) #a path
+summary(saved$model3) #b and c' path
+
+#total, direct, indirect effects
+saved$total.effect; saved$direct.effect; saved$indirect.effect
+
+#Sobel test
+saved$z.score; saved$p.value
+
+#bootstrapped indirect
+saved$boot.results
+
+#bootstrapped CI
+saved$boot.ci
