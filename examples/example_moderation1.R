5d493aa7e288bc36610b996b7b7c4652e7f91c82
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2018-06-29 22:59:14 -0400

    updated moderation example

--- examples/example_moderation1.R
+++ examples/example_moderation1.R
@@ -13,7 +13,8 @@ saved = moderation1(y = "Income", #DV
                     x = "Illiteracy", #IV
                     m = "Murder", #Moderator for simple slopes
                     cvs = c("Population", "Area"), #covariates
-                    df = states) #data frame of columns
+                    df = states, #data frame of columns
+                    with_out = T)
 
 #View the outliers
 View(saved$datascreening$fulldata)
@@ -21,7 +22,7 @@ View(saved$datascreening$fulldata)
 #Additivity/multicollinearity
 saved$datascreening$correl
 
-#Linearity 
+#Linearity
 saved$datascreening$linearity
 
 #Normality
@@ -33,14 +34,35 @@ saved$datascreening$homogen
 #Overall Model (Average Simple Slopes)
 summary(saved$model1)
 
+##overall model statistics
+#F(5, 44) = 10.30, p < .001, R2 = .54
+
+##each predictor one at a time
+#Illiteracy b = -114.78, t(44) = -0.67, p = .508, not significant predictor
+#Murder, also not signifificant
+#Population, as population increases, income increases
+#Area, as area increases, income increases
+#Interaction b = -115.56, t(44) = -3.36, p = .002 - significant
+#difficult to interpret...
+
 #Low Simple Slope Model
 summary(saved$model1low)
+
+#look for is X because M is the slope we are manipulating
+#Illiteracy not predictor of Income
 
 #High Simple Slope Model
 summary(saved$model1high)
 
+#Illiteracy is predictor of income
+
 #Interpretation of Slopes
 cat(saved$interpretation)
+
+##what does it mean if the interaction is significant but none of the simple
+##slopes are significant...??
+###implies the simple slopes are changing/different but that the main effect
+###X to Y is not significant
 
 #Graph of the Slopes
 saved$graphslopes
@@ -51,15 +73,20 @@ moderation1
 
 ####Power####
 library(pwr)
-##power runs on cohen's f - not to be confused with anova f. 
+##power runs on cohen's f - not to be confused with anova f.
 ##take the R squared to convert
-R2 =  .02
+R2 =  .11
 feta = R2 / (1-R2)
 
-#u is df model, which is number of predictors 
-#v is df error, but we are trying to figure out 
-#sample size so we leave this one blank. 
-#f2 is cohen f squared 
+#u is df model, which is number of predictors
+#v is df error, but we are trying to figure out
+#sample size so we leave this one blank.
+#f2 is cohen f squared
 
+##all the predictors
 pwr.f2.test(u = 5, v = NULL, f2 = feta, sig.level = .05, power = .80)
-#remember that you need to add u + v to get final sample size 
+#remember that you need to add u + v to get final sample size
+
+##addition of the interaction only
+pwr.f2.test(u = 1, v = NULL, f2 = feta, sig.level = .05, power = .80)
+#remember that you need to add u + v to get final sample size
